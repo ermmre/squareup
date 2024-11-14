e9e01f9a1958b6e683b5aebc66564e782d25a602
Pedro Sanchez Verdin <pedro2sanchez3@gmail.com>
2024-11-13 23:59:20 -0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <a href="/"><h1 className="name">SquareUp</h1></a>
+      </div>
+    </>
+  )
+}
+
+export default App
